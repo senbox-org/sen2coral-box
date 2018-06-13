08b7b59e5de3b52d25e939ad9c2dff1196b4e867
DC23 <jugglindan@gmail.com>
2018-06-13 14:58:35 +0800

    restricting to Python3 >= 3.5. Fixes #8

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ for Bathymetry, Un-mixing, and Concentration Assessment (SAMBUCA).''',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
