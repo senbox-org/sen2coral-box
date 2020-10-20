6bc1f0ce2aa946c5ef691498774bc95f37c656d2
Martino Ferrari <manda.mgf@gmail.com>
2020-10-20 13:41:39 +0200

    updated import

--- sen2coral-algorithms/src/main/java/org/esa/sen2coral/algorithms/RadiometricNormalisationPIFsOp.java
+++ sen2coral-algorithms/src/main/java/org/esa/sen2coral/algorithms/RadiometricNormalisationPIFsOp.java
@@ -1,11 +1,10 @@
 package org.esa.sen2coral.algorithms;
 
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LinearRing;
-import javafx.geometry.BoundingBox;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LinearRing;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.GeoCoding;
